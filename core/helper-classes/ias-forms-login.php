3f0dda4b8b4a051f879e7d04a6a05537eee380bf
Jack Michaels <jack@b54.com>
2014-05-01 17:36:49 +0300

    Close Bug #94

---
+++ core/helper-classes/ias-forms-login.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * IAS Login Form Class
+ */
+
+class ias_login_form extends ias_forms {
+	
+	function __construct( $layout ) {
+		
+	}
+
+	public static function widget( $layout ) {
+		
+	}
+
+	public static function shortcode( $layout ) {
+		
+	}
+} // end of ias_login_form class
+?>
